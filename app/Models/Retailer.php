d0526ccb02fdf78803a13004c748e5078b9a55f5
Mohamed Ismail <mohamed.ismail@zarcony.dev>
2022-08-06 17:40:19 +0200

    intial_relationships-completed

--- app/Models/Retailer.php
+++ app/Models/Retailer.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Retailer extends Model
 {
     use HasFactory;
+    public function addStock(Product $product, Stock $stock)
+    {
+        $stock->product_id = $product->id;
+        $this->stock()->save($stock);
+    }
+
+    public function stock()
+    {
+        return $this->hasMany(Stock::Class);
+    }
 }
